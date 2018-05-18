04b41b9d96d85776194ca02fdfa0d56c0825b70a
lpjiang97 <prestonj@cs.washington.edu>
2018-05-18 14:45:41 -0700

    add crosshair

---
+++ Graphics/Crosshair.py
@@ -0,0 +1,75 @@
+"""
+A crosshair, using wxPython
+"""
+
+import wx
+import time
+
+
+class Crosshair(wx.Frame):
+
+    def __init__(self, **kwargs):
+        """
+        A wxpython Crosshair that can flash red.
+
+        params:
+            - x_pos: x position (defaults to 1920 pixels left of main screen)
+            - y_pos: y position (defaults to 0)
+            - full_screen: if to use full screen, cannot be used together with width & height
+            - screen_width: size of screen in pixels
+            - screen_height: height of screen in pixels
+        """
+        wx.Frame.__init__(self, None, title="Crosshair")
+        # go full screen?
+        self.ShowFullScreen(kwargs.get("full_screen", False))
+        if kwargs.get("full_screen", False) and ("screen_width" in kwargs or "screen_height" in kwargs):
+            raise ValueError("Cannot use full screen and define width.height together!")
+        self.SetPosition((kwargs.get("x_pos", 100), kwargs.get("y_pos", 100)))
+        # some dependency
+        self.crosshair_width = kwargs.get("crosshair_width", 30)  
+        self.width_half = self.crosshair_width // 2
+        self.height_half = self.width_half
+        self.thickness = kwargs.get("pen_thickness", 5)
+        self.pen_color = wx.WHITE
+        self._update_dim()
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+    def _update_dim(self):
+        self.width, self.height = self.GetSize()
+        self.screenheight_half = self.height // 2
+        self.screenwidth_half = self.width // 2
+
+    def OnPaint(self, event=None):
+        """
+        Redraws the crosshair with the current pen color
+        """
+        self._update_dim()
+        dc = wx.PaintDC(self)
+        dc.Clear()
+        dc.SetPen(wx.Pen(self.pen_color, self.thickness))
+        # Draw Horizontal Crosshair
+        dc.DrawLine(self.screenwidth_half - self.width_half, self.screenheight_half,
+                    self.screenwidth_half + self.width_half, self.screenheight_half)
+        # Draw Horizontal Crosshair
+        dc.DrawLine(self.screenwidth_half, self.screenheight_half - self.height_half,
+                    self.screenwidth_half, self.screenheight_half + self.height_half)
+
+    def flash_red(self, duration=0.2):
+        """
+        Causes the crosshair to flash red for duration seconds.
+        :param duration: duration in seconds. defaults to 0.2
+        """
+        self.pen_color = wx.RED
+        self.Refresh(True)
+        t = time.time()
+        while time.time() - t < duration:
+            time.sleep(0.001)
+        self.pen_color = wx.WHITE
+        self.Refresh(True)
+
+
+if __name__ == '__main__':
+    app = wx.App(False)
+    ch = Crosshair(screen_width=500, screen_height=500)
+    ch.Show()
+    app.MainLoop()
